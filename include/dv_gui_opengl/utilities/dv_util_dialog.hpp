5945deb132b6f9dd4dd2579d1e892331b3e68487
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-02-11 16:28:46 +0100

    Add select dir and error message dialogs

--- include/dv_gui_opengl/utilities/dv_util_dialog.hpp
+++ include/dv_gui_opengl/utilities/dv_util_dialog.hpp
@@ -8,5 +8,9 @@ namespace dvsku {
     public:
         static std::vector<std::string> open_file(const std::string& title, const std::string& path, 
             std::vector<std::string> filters = { "All Files", "*" });
+
+        static std::string select_dir(const std::string& title, const std::string& path);
+
+        static void message_error(const std::string& title, const std::string& text);
     };
 }
--- src/dv_gui_opengl/utilities/dv_util_dialog.cpp
+++ src/dv_gui_opengl/utilities/dv_util_dialog.cpp
@@ -11,3 +11,11 @@ std::vector<std::string> dv_util_dialog::open_file(const std::string& title, con
 {
     return pfd::open_file(title, path, filters).result();
 }
+
+std::string dv_util_dialog::select_dir(const std::string& title, const std::string& path) {
+    return pfd::select_folder(title, path).result();
+}
+
+void dv_util_dialog::message_error(const std::string& title, const std::string& text) {
+    pfd::message(title, text, pfd::choice::ok, pfd::icon::error).result();
+}
